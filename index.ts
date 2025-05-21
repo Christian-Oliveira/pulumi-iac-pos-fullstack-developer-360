17e9777d1b6b6a82f1994e8c27c56053d7a85005
Christian Oliveira <christian.oliveira@luizalabs.com>
2025-05-21 20:01:38 -0300

    feat: criando segundo bucket na aws

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import * as aws from "@pulumi/aws";
 
 
-const bucket = new aws.s3.BucketV2("bucket-pos-rocketseat", {
-    bucket: 'bucket-pos-rocketseat',
+const firstBucket = new aws.s3.BucketV2("first-bucket-pos-rocketseat", {
+    bucket: 'first-bucket-pos-rocketseat',
+    tags: {
+        IAC: "true",
+    },
+});
+
+const secondBucket = new aws.s3.BucketV2("second-bucket-pos-rocketseat", {
+    bucket: 'second-bucket-pos-rocketseat',
     tags: {
         IAC: "true",
     },
@@ -17,9 +24,13 @@ const ecr = new aws.ecr.Repository("ecr-pos-rocketseat", {
 });
 
 
-export const bucketName = bucket.id;
-export const bucketRegion = bucket.region;
-export const bucketArn = bucket.arn;
+export const firstBucketName = firstBucket.id;
+export const firstBucketRegion = firstBucket.region;
+export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
